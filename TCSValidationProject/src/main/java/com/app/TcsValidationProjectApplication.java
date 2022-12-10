c92d8d28ff9e457d2306fe4f1d838691e3aa3a37
jvila <jvilalop@gmail.com>
2022-12-10 10:06:52 +0100

    New

--- TCSValidationProject/src/main/java/com/app/TcsValidationProjectApplication.java
+++ TCSValidationProject/src/main/java/com/app/TcsValidationProjectApplication.java
@@ -18,6 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TcsValidationProjectApplication implements CommandLineRunner {
     private final Logger logger = LoggerFactory.getLogger(TcsValidationProjectApplication.class);
     
+    
     @Autowired
     XMLValidatorService xmlValidatorService;
 	
