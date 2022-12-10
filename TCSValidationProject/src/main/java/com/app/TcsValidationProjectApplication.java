04b2f0a6b3523c64da252c4a0a5f0556ad8b2441
jvila <jvila@LAPTOP-PJHNOA4T>
2022-12-10 09:26:57 +0100

    Token

--- TCSValidationProject/src/main/java/com/app/TcsValidationProjectApplication.java
+++ TCSValidationProject/src/main/java/com/app/TcsValidationProjectApplication.java
@@ -18,7 +18,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TcsValidationProjectApplication implements CommandLineRunner {
     private final Logger logger = LoggerFactory.getLogger(TcsValidationProjectApplication.class);
     
-    
     @Autowired
     XMLValidatorService xmlValidatorService;
 	
